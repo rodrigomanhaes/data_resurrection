415a939e9dada5353e385e1521f3b5a33dd2dea7
Rodrigo Manhães <rmanhaes@gmail.com>
2011-08-18 02:12:26 -0300

    refactored DBF adapter

--- lib/data_resurrection/adapters.rb
+++ lib/data_resurrection/adapters.rb
@@ -5,26 +5,31 @@ module DataResurrection
   module Adapters
     module DBF
       def get_data(table_name, encodings=nil, sql_reserved_words=[])
-        table = ::DBF::Table.new(table_name)
-        result = table.map do |record|
-          table.columns.map {|c| { generated_field_name(c.name.downcase, sql_reserved_words) => record.send(c.name.downcase) }}.
-            reduce({}) {|h, e| h.merge! e }
-        end
-
-        if encodings
-          ic = Iconv.new(ENCODINGS[encodings.end], ENCODINGS[encodings.begin])
-          result.each do |record|
-            record.each do |k, v|
-              record[k] = ic.iconv(v) if v.kind_of?(String)
-            end
-          end
-        end
+        result = get_raw_data(table_name, sql_reserved_words)
+        result = handle_encodings(result, encodings) if encodings
         result
       end
 
       private
 
       ENCODINGS = {:utf8 => 'UTF-8', :win1252 => 'WINDOWS-1252'}
+
+      def get_raw_data(table_name, sql_reserved_words)
+        table = ::DBF::Table.new(table_name)
+        table.map do |record|
+          table.columns.map {|c| { generated_field_name(c.name.downcase, sql_reserved_words) => record.send(c.name.downcase) }}.
+            reduce({}) {|h, e| h.merge! e }
+        end
+      end
+
+      def handle_encodings(data, encodings)
+        ic = Iconv.new(ENCODINGS[encodings.end], ENCODINGS[encodings.begin])
+        data.each do |record|
+          record.each do |k, v|
+            record[k] = ic.iconv(v) if v.kind_of?(String)
+          end
+        end
+      end
 
       def generated_field_name(field_name, reserved_words)
         reserved_words.include?(field_name.upcase) ? "#{field_name}_" : field_name
